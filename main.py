40e4fa0fa1f60a4dd36367c81d05ec78c460da1e
HumzaTAhmad <humza3173@gmail.com>
2022-07-29 15:05:14 -0400

    Hide token from user view, run bot with hidden token Token was visible from github previously, so we created a .gitignore file to hide the token from public view

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import discord
 intents = discord.Intents.all()
 bot = discord.Client(intents = intents)
 
+with open('token.txt') as f:
+    TOKEN = f.readline()
+
 class main:
     def __init__(self):
         pass
 
 
 
-bot.run("MTAwMjM5NTI0NzkwMjQ3MDIyNg.GXBvv5.F506PBD3y9jQLtL9RzcOoVn-V0jM24dWmgwer0")
+bot.run(TOKEN)
