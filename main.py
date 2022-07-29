4d280e1faa26ede83076e4e5b73f73daa33b5344
HumzaTAhmad <humza3173@gmail.com>
2022-07-28 22:06:55 -0400

    Import Discord library, run DiscoBot, crate main class Imported the async ready API wrapper for Discord so we have a way to communicate with discord and set up events, commands, etc. Ran the bot through the bot token givent to us, this will ensure the bot is onlie and running. Created the main class as well, this will be the central cog wheel which will organize all of our functionalities from other files.

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord
+intents = discord.Intents.all()
+bot = discord.Client(intents = intents)
+
+class main:
+    def __init__(self):
+        pass
+
+
+
+bot.run("MTAwMjM5NTI0NzkwMjQ3MDIyNg.GXBvv5.F506PBD3y9jQLtL9RzcOoVn-V0jM24dWmgwer0")
