89ea98454e3e7a804b87be09e84e6101dc928a9b
HumzaTAhmad <humza3173@gmail.com>
2022-07-31 01:45:17 -0400

    Updated newadmin role by assigning the role even if no one has it previously
    Added this feature becasue the method would crash if no one else has the
    head admin role previously. This is because the list of members with
    this role would be empty and when accessing the empty list it would
    throw in error. Now if it is empty it'll just automatically add the role
    instead of trying to remove it from previous members

--- commands/moderation.py
+++ commands/moderation.py
@@ -36,23 +36,31 @@ class moderation(commands.Cog):
 
         admin_role = guild.get_role(1003116038839353365)
         old_admin_list = admin_role.members
-        old_admin = old_admin_list[0]
-        
-        await old_admin.remove_roles(admin_role, reason=None, atomic=True)
-        await new_admin.add_roles(admin_role, reason=None, atomic=True)
 
-        new_admin_pic = new_admin.avatar_url_as(size=1024)
-        server_logo = guild.icon_url_as(size=1024)
-        this_month_number = datetime.now().month
-        this_month_name = calendar.month_name[this_month_number]
+        if(len(old_admin_list) == 0):
+            await new_admin.add_roles(admin_role, reason=None, atomic=True)
+            await context.send(f"Gave the Head Admin role to @{new_admin}")
+        else:
+            old_admin = old_admin_list[0]
+            
+            await old_admin.remove_roles(admin_role, reason=None, atomic=True)
+            #old_admin, old_admin_discriminator = old_admin.split("#")
 
-        myEmbed = discord.Embed(title = f"HEAD ADMIN FOR {this_month_name.upper()}!!!", description = "This is an announcement declaring the new admin for this month", color = discord.Colour.dark_green())
-        myEmbed.add_field(name = "NAME", value = f"{new_admin} is now your new admin!", inline=False)
-        myEmbed.add_field(name = "WHY", value = f"{new_admin} Earned the support of the community, and as a result earned the most votes", inline=False)
-        myEmbed.set_image(url = new_admin_pic)
-        myEmbed.set_thumbnail(url = server_logo)
-        myEmbed.set_footer(text="Made by @Shxjo")
-        await context.send(embed = myEmbed)
+            await new_admin.add_roles(admin_role, reason=None, atomic=True)
+            #new_admin, new_admin_discriminator = new_admin.split("#")
+
+            new_admin_pic = new_admin.avatar_url_as(size=1024)
+            server_logo = guild.icon_url_as(size=1024)
+            this_month_number = datetime.now().month
+            this_month_name = calendar.month_name[this_month_number]
+
+            myEmbed = discord.Embed(title = f"HEAD ADMIN FOR {this_month_name.upper()}!!!", description = "@everyone: This is an announcement declaring the new admin for this month", color = discord.Colour.dark_green())
+            myEmbed.add_field(name = "NAME", value = f"@{new_admin} is now your new admin!", inline=False)
+            myEmbed.add_field(name = "WHY", value = f"@{new_admin} Earned the support of the community, and as a result earned the most votes", inline=False)
+            myEmbed.set_image(url = new_admin_pic)
+            myEmbed.set_thumbnail(url = server_logo)
+            myEmbed.set_footer(text="Made by @Shxjo")
+            await context.send(embed = myEmbed)
 
     @commands.group()
     async def edit(self, context):
