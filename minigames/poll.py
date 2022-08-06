f481fa9dc0b49faf0db3600f43fe8515186acaac
HumzaTAhmad <humza3173@gmail.com>
2022-08-06 15:36:33 -0400

    Fill poll command with proper functionality to create the poll
    allows the user to create polls in two way, one without options and one
    with options. This was done so the poll command would work even if no
    options were given. poll is sent out through an embed that allows for
    easier readabilty.

--- minigames/poll.py
+++ minigames/poll.py
@@ -9,7 +9,25 @@ class poll(commands.Cog):
     
     @commands.command()
     async def poll(self, context, minutes: int, title, *options):
-       
+        guild = context.guild
+        server_logo = guild.icon_url_as(size=1024)
+
+        if len(options) == 0:
+            embedPoll = discord.Embed(title = title, description = f"You have **{minutes}** minutes remaining!", color = discord.Colour.dark_green())
+            embedPoll.set_thumbnail(url = server_logo)
+            msg = await context.send(embed = embedPoll)
+            await msg.add_reaction("👍")
+            await msg.add_reaction("👎")
+
+        else:
+            embedPoll = discord.Embed(title = title, description = f"You have **{minutes}** minutes remaining!", color = discord.Colour.dark_green())
+            embedPoll.set_thumbnail(url = server_logo)
+            for number, option in enumerate(options):
+                embedPoll.add_field(name = f"{self.numbers[number]}", value = f"**{option}**", inline = False)
+            msg = await context.send(embed = embedPoll)
+
+            for x in range(len(embedPoll.fields)):
+                await msg.add_reaction(self.numbers[x])
 
 def setup(bot):
     bot.add_cog(poll(bot))
