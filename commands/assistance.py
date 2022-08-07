49bdb4341604bf499df7e88bc4dc18d2fda2050a
HumzaTAhmad <humza3173@gmail.com>
2022-08-07 15:43:59 -0400

    Clean up some code, move help command into assistance class
    moved the code around to make it more readable, deleted useless code

--- commands/assistance.py
+++ commands/assistance.py
@@ -10,44 +10,14 @@ class Assistance(commands.Cog):
         self.bot = bot
     
     @commands.command()
-    async def pages(self, context):
-        contents = ["This is page 1!", "This is page 2!", "This is page 3!", "This is page 4!"]
-        pages = 4
-        cur_page = 1
-        message = await context.send(f"Page {cur_page}/{pages}:\n{contents[cur_page-1]}")
-        # getting the message object for editing and reacting
-
-        await message.add_reaction("◀️")
-        await message.add_reaction("▶️")
-
-        def check(reaction, user):
-            return user == context.author and str(reaction.emoji) in ["◀️", "▶️"]
-            # This makes sure nobody except the command sender can interact with the "menu"
-
-        while True:
-            try:
-                reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
-                # waiting for a reaction to be added - times out after x seconds, 60 in this
-                # example
-
-                if str(reaction.emoji) == "▶️" and cur_page != pages:
-                    cur_page += 1
-                    await message.edit(content=f"Page {cur_page}/{pages}:\n{contents[cur_page-1]}")
-                    await message.remove_reaction(reaction, user)
-
-                elif str(reaction.emoji) == "◀️" and cur_page > 1:
-                    cur_page -= 1
-                    await message.edit(content=f"Page {cur_page}/{pages}:\n{contents[cur_page-1]}")
-                    await message.remove_reaction(reaction, user)
-
-                else:
-                    await message.remove_reaction(reaction, user)
-                    # removes reactions if the user tries to go forward on the last page or
-                    # backwards on the first page
-            except asyncio.TimeoutError:
-                await message.delete()
-                break
-                # ending the loop if user doesn't react after x seconds
+    async def help(self, context):
+        myEmbed = discord.Embed(title = "Commands", description = "These are the commands that you can use for this bot.", color = discord.Colour.dark_purple())
+        myEmbed.set_thumbnail(url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuWrXM1JcQ_CKMWg6PlvVSxioV6HKlLEB-yA&usqp=CAU")
+        myEmbed.add_field(name = "!edit servername", value = "This command allows you to change the server name", inline=False)
+        myEmbed.add_field(name = "!edit servername", value = "This command allows you to change the server name", inline=False)
+        #myEmbed.add_field(name = "!coinflip", value = "This Command lets you flip a coin", inline=False)
+        #myEmbed.add_field(name = "!RPS", value = "This command allows you to play a game of rock paper scissors with the bot.", inline=False)
+        await context.send(embed = myEmbed)
 
 def setup(bot):
     bot.add_cog(Assistance(bot))
--- main.py
+++ main.py
@@ -18,15 +18,6 @@ class Main:
     
     
     #function that returns the available commands that can be executed by the bot
-    @commands.command(aliases = ["about"])
-    async def help(self, context):
-        myEmbed = discord.Embed(title = "Commands", description = "These are the commands that you can use for this bot.", color = discord.Colour.dark_purple())
-        myEmbed.set_thumbnail(url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuWrXM1JcQ_CKMWg6PlvVSxioV6HKlLEB-yA&usqp=CAU")
-        myEmbed.add_field(name = "!edit servername", value = "This command allows you to change the server name", inline=False)
-        myEmbed.add_field(name = "!edit servername", value = "This command allows you to change the server name", inline=False)
-        #myEmbed.add_field(name = "!coinflip", value = "This Command lets you flip a coin", inline=False)
-        #myEmbed.add_field(name = "!RPS", value = "This command allows you to play a game of rock paper scissors with the bot.", inline=False)
-        await context.send(embed = myEmbed)
 
 
     bot.load_extension("commands.moderation")
